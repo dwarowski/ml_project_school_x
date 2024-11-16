eaff4faa9185641fa404e5d8b15d96d9705d3352
dwarowski <realcarenthusiast@yandex.ru>
2024-11-16 19:07:15 +0300

    final

--- src/Components/StartPage/Main.jsx
+++ src/Components/StartPage/Main.jsx
@@ -1,5 +1,5 @@
-import bg from './img/background.png';
 import logo from './img/Vector.svg'
+import contact from './img/contact.png'
 import './Main.css';
 
 function App() {
@@ -10,19 +10,19 @@ function App() {
           <div className="logo-box">
             <img src={logo} className="logo" alt="logo"/>
           </div>
-          <div className="button">
+          <div className="contact-us-button">
+            <img src={contact} className="contact-us-icon" alt='contact-us'></img>
             <p className='contact-us-text'>Связаться с нами</p>
           </div>
         </div>
         <div className="header-info-container">
           <p className="main-text">Адаптация текстов</p>
           <p className="info-text">Сервис для адаптации текстов под уровни CEFR. Быстро и легко преобразуйте тексты для нужного уровня сложности</p>
-          <div className="button">
+          <div className="start-button">
             <p className="start-button-text">Начать работу</p>
           </div>
         </div>
       </div>   
-      <img src={bg} className="bg-image" alt="background"/>
     </div>
     
   );
