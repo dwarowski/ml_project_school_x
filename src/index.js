43dbd5d322564084cdd8a7291f3cc05067590b16
dwarowski <realcarenthusiast@yandex.ru>
2024-11-20 21:08:15 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import reportWebVitals from './reportWebVitals.js';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Start/>
     <MainPage/>
   </React.StrictMode>
 );
