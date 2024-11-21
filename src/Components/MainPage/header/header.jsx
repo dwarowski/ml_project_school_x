e277ec77010662daaab14902dc901feb6cb70b70
grin_leo <gridchin.leo@gmail.com>
2024-11-21 17:57:10 +0300

    DesignFix

--- src/Components/MainPage/header/header.jsx
+++ src/Components/MainPage/header/header.jsx
@@ -11,14 +11,14 @@ export default function Header(){
     return(
 
         <div className='header'>
-        <img src={logo} alt="" className='icon' />
+          <img src={logo} alt="" className='icon' />
 
-        <div className='func_block'>
-          <img src={acc} alt="" className='icon' />
-          <img src={history} alt="" className='icon'/>
+          <div className='func_block'>
+            <img src={acc} alt="" className='icon' />
+            <img src={history} alt="" className='icon' />
 
-          <div className='contacts'>Связаться с нами</div>
-        </div>
+            <div className='contacts'>Связаться с нами</div>
+          </div>
 
       </div>
 
