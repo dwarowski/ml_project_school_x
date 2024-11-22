3ac9811d24d46e43efb9d78d459cc6e8a78996a7
dwarowski <realcarenthusiast@yandex.ru>
2024-11-22 16:18:39 +0300

    added conctact us buton to error page

--- src/Components/ErrorPage/ErrorPage.jsx
+++ src/Components/ErrorPage/ErrorPage.jsx
@@ -1,14 +1,21 @@
 import { useRouteError } from "react-router-dom";
 import "./ErrorPage.css"
+import { Link } from 'react-router-dom'
+
 
 export default function ErrorPage() {
   const error = useRouteError();
   console.error(error);
 
   return (
-    <div id="error-page" className="error">
-      <h1>Упс!</h1>
-      <p>Что-то пошло не так</p>
+    <div className="error">
+      <h1 className="oops">Упс!</h1>
+      <p className="fail">Что-то пошло не так</p>
+      <Link to="https://t.me/JapnaeseMadeThings" className='text-link contact-us-link'>
+        <div className="error-contact-us-button">
+          <p className="error-contact-us-text">Связаться с нами</p>
+        </div>
+      </Link>
       <p>
         <i>{error.statusText || error.message}</i>
       </p>
