e0ad29983b4d5612eb7ed5b9c09ad79c6c1de8dd
dwarowski <realcarenthusiast@yandex.ru>
2024-11-22 15:40:12 +0300

    fixed adaptation and refactored again

--- src/Components/StartPage/Start.jsx
+++ src/Components/StartPage/Start.jsx
@@ -8,26 +8,28 @@ import './Start.css';
 function App() {
   return (
     <div className="main-container">
-      <div className="main-elements-container">
-        <div className="upper-container">
-          <div className="logo-box">
-            <img src={logo} className="logo" alt="logo"/>
+      <div className="background">
+        <div className="elements-container">
+          <div className="header-container">
+            <div className="logo-container">
+              <img src={logo} className="logo" alt="logo"/>
+            </div>
+            <Link to="https://t.me/JapnaeseMadeThings" className='text-link contact-us-link'>
+              <div className="button contact-us-button">
+                <img src={contact} className="contact-us-icon" alt='contact-us'></img>
+                <p className='button-text contact-us-text'>Связаться с нами</p>
+              </div>
+            </Link>
           </div>
-          <Link to="https://t.me/JapnaeseMadeThings" className='text-link'>
-            <div className="button contact-us-button">
-              <img src={contact} className="contact-us-icon" alt='contact-us'></img>
-              <p className='text contact-us-text'>Связаться с нами</p>
-            </div>
-          </Link>
-        </div>
-        <div className="header-info-container">
-          <p className="text main-text">Адаптация текстов</p>
-          <p className="text info-text">Сервис для адаптации текстов под уровни CEFR. Быстро и легко преобразуйте тексты для нужного уровня сложности</p>
-          <Link to='mainpage'  className='text-link'>
-            <div className="button start-button">
-              <p className="text start-button-text">Начать работу</p>
-            </div>
-          </Link>
+          <div className="info-container">
+            <p className="text main-info-text">Адаптация текстов</p>
+            <p className="text sub-info-text">Сервис для адаптации текстов под уровни CEFR. Быстро и легко преобразуйте тексты для нужного уровня сложности</p>
+            <Link to='mainpage'  className='text-link start-link'>
+              <div className="button start-button">
+                <p className="button-text start-button-text">Начать работу</p>
+              </div>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
