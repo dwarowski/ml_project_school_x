d4e6d5edafe2d670b6406d2520129b54634b1076
dwarowski <realcarenthusiast@yandex.ru>
2024-11-25 14:26:14 +0300

    added icon instead of text on mobile

--- src/Components/MainPage/header/header.jsx
+++ src/Components/MainPage/header/header.jsx
@@ -1,9 +1,9 @@
 import "./header.scss"
-
+import { Link } from 'react-router-dom'
 import logo from "../../../assets/logo.svg";
 import acc from "../../../assets/acc.svg";
 import history from "../../../assets/history.svg";
-
+import contact from '../../../assets/img/contact.png'
 
 
 
@@ -16,8 +16,12 @@ export default function Header(){
           <div className='func_block'>
             <img src={acc} alt="" className='icon' />
             <img src={history} alt="" className='icon' />
-
+            
+            
             <a className='contacts' href="https://telegram.me/GRIN_L" target="_blank" rel="noopener noreferrer" >Связаться с нами</a>
+            <Link to="https://telegram.me/GRIN_L">
+            <img src={contact} className="icon contacts-us-icon" alt='contact-us' href="https://telegram.me/GRIN_L"/>
+            </Link>
           </div>
 
       </div>
