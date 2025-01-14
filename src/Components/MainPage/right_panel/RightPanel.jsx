407d4380d17c1d7d0ab3fc8cfa6a6ad5ca7b044d
grin_leo <gridchin.leo@gmail.com>
2025-01-14 20:35:52 +0300

    copy button

--- src/Components/MainPage/right_panel/RightPanel.jsx
+++ src/Components/MainPage/right_panel/RightPanel.jsx
@@ -5,41 +5,69 @@ import { useState, useEffect } from "react";
 
 import copy from "../../../assets/copy.svg";
 
-
 export default function RightPanel() {
   const { activeLvlButtons, startLvlText, adaptiveText } = useAppContext();
 
-  const [showAdaptText, setShowAdaptText] = useState(false)
+  const [showAdaptText, setShowAdaptText] = useState(false);
   const [shake, setShake] = useState(false); // состояние анимации
 
-
-
   useEffect(() => {
-    const hasActiveButtonLvl = activeLvlButtons.some(button => button === true);
+    const hasActiveButtonLvl = activeLvlButtons.some((button) => button === true);
     if (hasActiveButtonLvl) {
-      setShowAdaptText(true)
+      setShowAdaptText(true);
     }
-  },
-    [activeLvlButtons, adaptiveText]);
+  }, [activeLvlButtons, adaptiveText]);
 
+  const handleCopyText = () => {
+    if (adaptiveText) {
+      navigator.clipboard
+        .writeText(adaptiveText)
+        .then(() => {
+          console.log("Text copied to clipboard");
+        })
+        .catch((err) => {
+          console.error("Failed to copy text: ", err);
+        });
+    }
+  };
 
   return (
-    <div className='panel_part'>
-      <div className='fake_area'>
-        <div className="basic-text" style={{ display: `${showAdaptText ? "none" : ""}`, color: `${startLvlText ? '#0F2450' : '#717171;'}` }}>Выберите уровень<br />для адаптации</div>
-        <div className='title' style={{ display: `${showAdaptText ? "" : "none"}` }}>АДАПТИРОВАННЫЙ ТЕКСТ</div>
-        <div className='line' style={{ display: `${showAdaptText ? "" : "none"}` }}></div>
-        <div className="read-container" style={{ height: `${showAdaptText ? "70%" : "33.5%"}` }}>
-          <div className="read-only"> {adaptiveText}</div>
-          <button className="copy-button">
-            <img src={copy} alt="" style={{ display: `${showAdaptText ? 'block' : 'none'}` }} />
+    <div className="panel_part">
+      <div className="fake_area">
+        <div
+          className="basic-text"
+          style={{
+            display: `${showAdaptText ? "none" : ""}`,
+            color: `${startLvlText ? "#0F2450" : "#717171;"}`,
+          }}
+        >
+          Выберите уровень
+          <br />
+          для адаптации
+        </div>
+        <div className="title" style={{ display: `${showAdaptText ? "" : "none"}` }}>
+          АДАПТИРОВАННЫЙ ТЕКСТ
+        </div>
+        <div className="line" style={{ display: `${showAdaptText ? "" : "none"}` }}></div>
+        <div
+          className="read-container"
+          style={{ height: `${showAdaptText ? "70%" : "33.5%"}` }}
+        >
+          <div className="read-only">{adaptiveText}</div>
+          <button
+            className="copy-button"
+            onClick={handleCopyText} // Обработчик клика
+          >
+            <img
+              src={copy}
+              alt=""
+              style={{ display: `${showAdaptText ? "block" : "none"}` }}
+            />
           </button>
-
         </div>
         <div className={`buttons_block ${shake ? "shake" : ""}`}>
           <LvlButton />
         </div>
-
       </div>
     </div>
   );
